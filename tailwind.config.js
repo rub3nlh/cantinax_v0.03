ce36d044f9c5335977c211749bf07afe8b862778
Ruben Lang Herrera <ruben.lh@gmail.com>
2025-05-05 18:01:33 +0200

    changing color base

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#EB4A3E',
+        'primary-foreground': '#ffffff',
+        red: {
+          500: '#EB4A3E', // Replacing the default red-500 with #EB4A3E
+        }
+      },
+    },
   },
   plugins: [],
 };
